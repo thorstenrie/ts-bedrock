9d63ad249cf6f238e13ffe5ab32b241da8f2d82e
thorstenrie <author@lichtprotoss.com>
2022-12-26 00:04:26 +0100

    Add custom server config files

--- bedrock/start.sh
+++ bedrock/start.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# Unzip bedrock server
+unzip -q ../bedrock-server.zip -d .
+rm ../bedrock-server.zip
+
+# Copy custom config files, if any
+for f in ../config/{server.properties,permissions.json,allowlist.json}
+do
+    if [ -e "$f" ]
+        then cp "$f" .
+    fi
+done
+
 # Export LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=.
 
